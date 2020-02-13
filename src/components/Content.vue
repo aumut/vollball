e656c8dc01f3f44251cff8ae909dc062bf317faa
a_umut <askar.umut.kal@gmail.com>
2020-02-13 10:07:11 +0600

    html to vue

---
+++ src/components/Content.vue
@@ -0,0 +1,36 @@
+<template>
+    <content>
+        <div class="page" v-for="page in page_list" v-bind:key="page.index">
+            <h2 class="page__title">{{ page.title }}</h2>
+            <div class="page__inner">
+                <component :is="page.section"></component>
+            </div>
+        </div>
+    </content>
+</template>
+<script>
+    import ContentNews from './ContentNews.vue';
+    import ContentTimeTable from './ContentTimeTable.vue';
+    export default {
+        name: 'Content',
+        components: {
+            ContentNews, ContentTimeTable
+        },
+        data(){
+            return {
+                page_list: [
+                    {
+                        index: 1,
+                        title: 'Новости',
+                        section: ContentNews
+                    },
+                    {
+                        index: 2,
+                        title: 'Расписание',
+                        section: ContentTimeTable
+                    }
+                ]
+            }
+        }
+    }
+</script>
---
+++ src/components/ContentNews.vue
@@ -0,0 +1,66 @@
+<template>
+    <div>
+        <div class="article-list" v-for="news in news_list" v-bind:key="news.index">
+            <div class="article-list__img"><img v-bind:src="require('../assets/images/content/' + news.img)"></div>
+            <div class="article-list__info">
+                <h3 class="article-list__title mb-2"><a href="#">{{ news.title}}</a></h3>
+                <h4 class="article-list__subtitle mb-2"><a href="#">{{ news.subtitle }}</a></h4>
+                <div class="article-list__details">
+                    <span class="article-list__date">{{ news.date }}</span>
+                    <span class="article-list__tag">{{ news.tag}}}</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'ContentNews',
+        data() {
+            return {
+                news_list: [
+                    {
+                        index: 1,
+                        img: 'slider_1.jpg',
+                        title: 'Россия это сделала! Наши волейболисты смели всех и прорвались на Олимпиаду',
+                        subtitle: 'Сборная России обыграла команду Ирана в трёх сетах. Соперники бились отчаянно, но устоять в Санкт-Петербурге не смогли.',
+                        date: '6 сентября, 22:15',
+                        tag: ' Сборная Россия'
+                    },
+                    {
+                        index: 2,
+                        img: 'slider_1.jpg',
+                        title: 'Россия это сделала! Наши волейболисты смели всех и прорвались на Олимпиаду',
+                        subtitle: 'Сборная России обыграла команду Ирана в трёх сетах. Соперники бились отчаянно, но устоять в Санкт-Петербурге не смогли.',
+                        date: '6 сентября, 22:15',
+                        tag: ' Сборная Россия'
+                    },
+                    {
+                        index: 3,
+                        img: 'slider_1.jpg',
+                        title: 'Россия это сделала! Наши волейболисты смели всех и прорвались на Олимпиаду',
+                        subtitle: 'Сборная России обыграла команду Ирана в трёх сетах. Соперники бились отчаянно, но устоять в Санкт-Петербурге не смогли.',
+                        date: '6 сентября, 22:15',
+                        tag: ' Сборная Россия'
+                    },
+                    {
+                        index: 4,
+                        img: 'slider_1.jpg',
+                        title: 'Россия это сделала! Наши волейболисты смели всех и прорвались на Олимпиаду',
+                        subtitle: 'Сборная России обыграла команду Ирана в трёх сетах. Соперники бились отчаянно, но устоять в Санкт-Петербурге не смогли.',
+                        date: '6 сентября, 22:15',
+                        tag: ' Сборная Россия'
+                    },
+                    {
+                        index: 5,
+                        img: 'slider_1.jpg',
+                        title: 'Россия это сделала! Наши волейболисты смели всех и прорвались на Олимпиаду',
+                        subtitle: 'Сборная России обыграла команду Ирана в трёх сетах. Соперники бились отчаянно, но устоять в Санкт-Петербурге не смогли.',
+                        date: '6 сентября, 22:15',
+                        tag: ' Сборная Россия'
+                    }
+                ]
+            }
+        }
+    }
+</script>
