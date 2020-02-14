ff42cb0fb1451981afc00850f88670b6c9f59533
ivanv <mrsmith.tallahassee@gmail.com>
2020-02-14 13:45:40 +0600

    calendar inital commit

---
+++ src/components/CalendarRender.vue
@@ -0,0 +1,24 @@
+<template>
+    <table>
+        <th>
+            <td>{{ month + "/" + year }}</td>
+        </th>
+    </table>
+</template>
+
+<script>
+export default {
+    name: 'CalendarRender',
+    props: {
+        'month': Number,
+        'year': Number,
+        'events': Array
+    },
+    data: function() { return {
+        
+    }},
+    methods: {
+
+    }
+}
+</script>
---
+++ src/components/CalendarWrap.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <cal-rend/>
+    </div>
+</template>
+
+<script>
+import calRend from "./CalendarRender"
+export default {
+    name: 'CalendarWrap',
+    components: { calRend },
+    data: function() { return {
+        month: 0,
+        year: 2020,
+        events: []
+    }},
+    methods: {
+
+    }
+}
+</script>
